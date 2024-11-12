6e839141151b1f690d6618f9e138363b4ce1ee2f
ven1nom <your-email@example.com>
2024-11-12 21:20:59 +0530

    "feed api"

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,9 @@ const { userAuth } = require('../middlewares/userAuth');
 const userRouter=express.Router();
 const {userAuth}=require("../middlewares/userAuth");
 const ConnectionRequest = require('../model/connectionRequest');
+const User=require('../model/user')
+
+
 //APIs
 //ALL connection for login User
 // GET all pending connection for login User 
@@ -58,4 +61,68 @@ userRouter.get('/user/connections',userAuth,async (req,res)=>{
         res.status(400).send({message:err.message})
     }
 })
+//query params => feed?page=1&limit=10 pass in postman
+userRouter.get('/user/feed', userAuth, async (req,res)=>{
+    //User shouldnt see card of someone
+    //whom -> sent or recieved request
+    // User shouuldnt see his card himself    
+  
+   //Our DB for new user will showcase all user
+   //But we need , only few users 
+  const page=parseInt(req.query.page) || 1;
+  const limit=parseInt(req.query.limit)  || 10;
+  //sanitization on query param
+  limit=limit>50?50:limit;
+  
+  // assume page number is one
+  //skip page number formula
+  const skip=(page-1)*limit;
+
+
+    const loggedInUser=req.user;
+    try{
+      
+      // either loggedInUser has sent or accepted connection request we will see them as toUser or fromUser
+     // first access all such request then do "negative" operation
+      const connectionRequests=await ConnectionRequest.find({
+        $or:[{
+            fromUserId:loggedInUser._id
+        },{toUserId:loggedInUser._id}]
+
+     }).select("fromUserId toUserId")
+     //"select"=> we only want from and to from this   
+
+     //block this users
+     // we want to handle duplicate
+     const hideUsersFromFeed=new Set();
+     connectionRequests.forEach((req)=>{
+        hideUsersFromFeed.add(req.fromUserId.toString())
+        hideUsersFromFeed.add(req.toUserId.toString())
+    }) 
+
+    console.log(hideUsersFromFeed);
+    // these are 4 people I wish to hide from feed
+     
+    //Wish to send all users except hideUsers
+    const users=await User.find({
+        $and:[
+      {_id:{$nin: Array.from(hideUsersFromFeed)}
+    },
+    {_id:{$ne:loggedInUser._id}},]   
+    }).select("firstName","lastName","photoUrl","age","about","skills","gender").skip(skip).limit(limit);
+    res.send(users)
+     
+     //Shouldnt send to himself -> toUserId != fromUserdId and vice-versa
+    }catch(err){
+        res.status(400).send("Unable to load feed")
+    }
+})
+
 module.exports={userRouter};
+//pagination & query params
+// query params && mongoDB
+// /feed?page=1&limit=10-> 1-10  ===> .skip(0) & .limit(10)
+// /feed?page=2&limit=10=>11-20  ===> .skip(10) & .limit(10)
+
+//.skip(0) & .limit(10)
+// first 10 users - skipping 0 users and limit 10 users
